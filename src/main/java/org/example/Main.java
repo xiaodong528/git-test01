02d5f1d319e595aaf54ad9e3d560a6b33d1eb4c2
sxjcz <sxjczyd@163.com>
2023-03-03 21:46:16 +0800

    slave-secondTest01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,5 +16,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello slave!");
+        System.out.println("Hello slave!");
     }
 }
